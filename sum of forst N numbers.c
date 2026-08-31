02c24e647203203ab31616a822cd6de81354f367
harshita purohit <harshitapurohit2807@gmail.com>
2026-08-30 23:29:37 -0700

    Add files via upload

---
+++ sum of forst N numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum;
+    printf("enter number");
+    scanf("%d",&n);
+
+      sum=0;
+      for(i=1;i<=n;i++)
+      {
+          sum=sum+i;
+
+      }
+       printf("%d",sum);
+
+
+      return 0;
+}
